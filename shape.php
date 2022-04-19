be4587760bad56823d892c58d294ffc9f34611c2
JC.SomeCodes <90760930+JCsomeShots@users.noreply.github.com>
2022-04-19 20:59:32 +0200

    Update shape.php

--- shape.php
+++ shape.php
@@ -2,20 +2,12 @@
 
 class Shape{
 
-	public $ancho;
-	public $alto;
+	protected $ancho;
+	protected $alto;
 
 	public function __construct($ancho , $alto){
 		$this-> ancho = $ancho;
-		$this -> alto = $alto;
-	}
-
-	public function areaTriangulo (){
-		echo "El área del triángulo de ". $this->ancho. " de ancho por ". $this->alto ." de alto sera : ". (($this->ancho * $this->alto)/2);
-	}
-
-	public function areaRectangulo(){
-		echo "El área del rectángulo de ". $this->ancho. " de ancho por ". $this->alto ." de alto sera : ". ($this->ancho * $this->alto);
+		$this-> alto = $alto;
 	}
 } 
 
