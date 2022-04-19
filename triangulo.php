a7d5d91835cc340e74447e0ed74e98eac70b2b1f
JC.SomeCodes <90760930+JCsomeShots@users.noreply.github.com>
2022-04-19 20:58:18 +0200

    Update triangulo.php

--- triangulo.php
+++ triangulo.php
@@ -1,4 +1,13 @@
 <?php 
-class Triangle extends Shape {}
+class Triangle extends Shape {
+       public function areaTriangulo (){
+          return ($this->ancho * $this->alto)/2;
+        }
+  
+        public function areaTrianguloImprimir (){
+          echo "El área del triángulo de ". $this->ancho. " de ancho por ". $this->alto ." de alto sera : ".  $this->areaTriangulo();
+        }
+
+}
 ?>
 
