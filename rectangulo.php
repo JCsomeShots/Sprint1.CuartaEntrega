c6f35cbe5795d1876b69a2d57aba95ab342ed049
JC.SomeCodes <90760930+JCsomeShots@users.noreply.github.com>
2022-04-20 14:51:20 +0200

    Update rectangulo.php

--- rectangulo.php
+++ rectangulo.php
@@ -1,14 +1,10 @@
 <?php 
-class Rectangulo extends Shape {
-  
-    public function areaRectangulo()
-    {
-        return $this->ancho * $this->alto;
-    }
-  
-    public function areaRectanguloImprimir()
-    {
-        echo "El área del triángulo de ". $this->ancho. " de ancho por ". $this->alto ." de alto sera : ".  $this->areaRectangulo();
-    }
+class Rectangulo extends Shape
+{
+    public $forma = 'rectangle';
+    
+    // public function area (){
+    // 	  return parent::area();
+	  // }
 }
 ?>
