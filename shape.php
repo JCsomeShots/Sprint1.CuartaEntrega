52a419232dabe6b462dcf9486b31bbcff988dab8
JC.SomeCodes <90760930+JCsomeShots@users.noreply.github.com>
2022-04-20 14:54:01 +0200

    Update shape.php

--- shape.php
+++ shape.php
@@ -2,13 +2,18 @@
 
 class Shape{
 
-	protected $ancho;
-	protected $alto;
+	public $ancho;
+	public $alto;
 
 	public function __construct($ancho , $alto){
 		$this-> ancho = $ancho;
 		$this-> alto = $alto;
 	}
-} 
+	
+	public function area()
+    	{
+       		 return $this->ancho * $this->alto;
+    	}
+}
 
 ?>
